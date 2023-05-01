566399b36f993478fcf3bca4fa7bf3ffe293f6ed
DESKTOP-JFSBM9Q\Sinke <gsussman55@gmail.com>
2023-05-01 15:49:08 -0400

    Connect Client and Server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.length("/api", (req, res) => {
-    res.json({"test": "testPass"})
+app.get("/api", (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.json("test")
 })
 
 app.listen(5000, () => {console.log("Server started on port 5000")})
