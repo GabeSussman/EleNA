89ccbde0aa37e6a7ec3e0a3f57ade92d21c4e65e
howiechan123 <113634531+howiechan123@users.noreply.github.com>
2023-05-14 20:10:09 -0400

    map display

--- client/src/Components/Map.js
+++ client/src/Components/Map.js
@@ -1,10 +1,39 @@
 import React, { useState } from "react";
 import Header from './Header';
 import "./map.css";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Polyline } from "react-leaflet";
 import { useRef } from "react";
 import "leaflet/dist/leaflet.css";
 
+
+
+var path;
+var start = path[0];
+var end = path[path.length];
+
+
+function setPath(p) {
+    path = p;
+}
+
+function getPath() {
+    return coordinates;
+}
+
+function setStart(coords) {
+    start = coords;
+}
+
+function setEnd(coords) {
+    end = coords;
+}
+
+
+var coordinates = [
+    [42.391155, -72.5199],
+    [42.4,-72.5]
+
+];
 
 const BasicMap = () => {
     const [center, setCenter] = useState({ lat: 42.391155, lng: -72.5199 });
@@ -18,6 +47,7 @@ const BasicMap = () => {
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                <Polyline positions={getPath()} color = "red" />
             </MapContainer>
         </div>
 
