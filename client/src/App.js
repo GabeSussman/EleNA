b793f3c333ca22aa9543711b834dd355746e1842
howiechan123 <113634531+howiechan123@users.noreply.github.com>
2023-05-22 23:15:22 -0400

    accepted changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-<<<<<<< HEAD
-import {useEffect, useState} from 'react'
-import Sidebar from './components/sidebar'
-=======
 import {useEffect, useState} from 'react';
 import Sidebar from './components/sidebar';
 import Map from "./components/Map";
 import Page from "./components/page";
 import { faCrosshairs } from "@fortawesome/free-solid-svg-icons";
->>>>>>> howie
 
 function App() {
 
@@ -27,11 +22,7 @@ function App() {
   return (
 
     <div className="App">
-<<<<<<< HEAD
-      <Sidebar />
-=======
       <Page />
->>>>>>> howie
     </div>
 
   );
