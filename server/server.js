6891f82626dba39d6117f33562cb9eedadb6fb34
howiechan123 <113634531+howiechan123@users.noreply.github.com>
2023-05-23 22:23:03 -0400

    changed server port to 5001

--- server/server.js
+++ server/server.js
@@ -76,7 +76,7 @@ app.get("/routes/:startLat/:startLon/:endLat/:endLon/:choice/:percent", (req, re
 // })()
 
 
-app.listen(5000, () => {console.log("Server started on port 5000")})
+app.listen(5001, () => {console.log("Server started on port 5001")})
 
 function distChange(path){
     let e = 0
